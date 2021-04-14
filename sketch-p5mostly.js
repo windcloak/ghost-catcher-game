fa4701c079584b503bca6a8a2dbd38b55cf08719
May Guo <40670609+windcloak@users.noreply.github.com>
2021-04-14 00:46:48 -0500

    added controller arduino input

--- sketch-p5mostly.js
+++ sketch-p5mostly.js
@@ -1,5 +1,5 @@
 // Xinmei Guo
-//
+// 
 
 // To play the game, click the screen when you see a blue or purple color
 // Do not click if you see another color (you will lose a point!)
@@ -13,7 +13,7 @@
 // Audio - if you get a HIT, you will hear a high music note (ToneJS).
 // If you MISS, you hear a lower note
 
-let noiseSynth, noise
+let noiseSynth
 let colors = [
   'magenta',
   'aquamarine',
@@ -70,19 +70,10 @@ let isCorrect = false,
   isCarryingGhost = false
 const interval = 60 // interval for when color changes
 let feedback // feedback if player got a HIT or MISS
-let ghostHappy, ghostSad, ghostCursor, netCursor // different ghost pics depending on score
-
-// Arduino
-let x_position = 0,
-  y_position = 0
-let buttonCheck
-let isArduino = false
+let ghostHappy, ghostSad // different ghost pics depending on score
 
 // coordinates of jar
-const jarX1 = 230,
-  jarY1 = 230,
-  jarX2 = 540,
-  jarY2 = 495
+const jarX1 = 230, jarY1 = 230, jarX2 = 540, jarY2 = 495
 
 // Declare a "SerialPort" object
 var serial
@@ -91,9 +82,6 @@ var portName = 'COM3' // fill in your serial port name here
 var outMessage = 'H'
 
 function setup() {
-  
-  noise = new Tone.NoiseSynth().toDestination();
-  
   currentColor = colors[2]
   createCanvas(windowWidth * 0.98, windowHeight * 0.98)
   background(currentColor)
@@ -110,25 +98,11 @@ function setup() {
   // Assuming our Arduino is connected,  open the connection to it
   serial.open(portName)
 
-  // When we connect to the underlying server
-  serial.on('connected', serverConnected)
-
   // When you get a list of serial ports that are available
   serial.on('list', gotList)
 
   // When you some data from the serial port
   serial.on('data', gotData)
-
-  // When or if we get an error
-  serial.on('error', gotError)
-
-  // When our serial port is opened and ready for read/write
-  serial.on('open', gotOpen)
-}
-
-// We are connected and ready to go
-function serverConnected() {
-  print('Connected to Server')
 }
 
 // Got the list of ports
@@ -143,53 +117,30 @@ function gotList(thelist) {
 
 // Called when there is data available from the serial port
 function gotData() {
-  let currentString = serial.readLine() // read the incoming string
-  trim(currentString) // remove any trailing whitespace
-  if (!currentString) return // if the string is empty, do no more
-  let arduino_input = currentString.split(',')
-
-  x_position = parseInt(arduino_input[0])
-  y_position = parseInt(arduino_input[1])
-  let b = arduino_input[2]
-  // convert raw data (-512 to 512) to our canvas resolution
-  x_position = ((x_position + 512) / (512 + 512)) * width
-  y_position = ((y_position + 512) / (512 + 512)) * height
-  console.log(x_position)
-  console.log(y_position)
-  if (b == 1) {
-    buttonCheck = false
-  } else {
-    buttonCheck = true
-  }
-  isArduino = true
-}
-
-// Connected to our serial device
-function gotOpen() {
-  print('Serial Port is Open')
-}
-
-// Ut oh, here is an error, let's log it
-function gotError(theerror) {
-  print(theerror)
+  var currentString = serial.readLine()
+  console.log(currentString)
 }
 
 function preload() {
   // preload multimedia
   ghostHappy = loadImage('ghost-happy.png')
   ghostSad = loadImage('ghost-sad.png')
-  ghostCursor = loadImage('ghost-cursor.png')
-  netCursor = loadImage('net.png')
 }
 
 function keyPressed() {
   console.log(keyCode)
   if (keyCode == 32) {
-    startGame()
+    isStart = false
   }
 }
 
 function draw() {
+  // cursor
+  if (isCarryingGhost) {
+    cursor('ghost-cursor.png')
+  } else {
+  cursor('net.png')
+  }
   changeBackgroundColor()
   background(currentColor)
 
@@ -200,7 +151,7 @@ function draw() {
     feedback = 'GOOD! Move it into the jar!'
   } else if (isCorrect) {
     feedback = 'GOOD!'
-  } else if (!isCorrect & (attempts > 0)) {
+  } else if (!isCorrect & attempts>0) {
     feedback = 'MISS!'
   } else {
     feedback = 'Try to catch a ghost!'
@@ -219,7 +170,11 @@ function draw() {
       width / 3,
       150,
     )
-    text('Then drag the ghost into the jar', width / 3, 200)
+    text(
+      'Then drag the ghost into the jar',
+      width / 3,
+      200,
+    )
     text('You lose 1 point if you click the wrong color', width / 3, 250)
 
     textSize(35)
@@ -236,20 +191,9 @@ function draw() {
     } else {
       image(ghostSad, 200, 100)
     }
-
-    changeCursor()
   } else if (isStart && !isEnd) {
     textSize(35)
-    text(
-      `Press Spacebar or Arduino button to start Ghost Catcher Game`,
-      width / 4,
-      100,
-    )
-    text(
-      `You must use Arduino button and controller if your Arduino is connected!`,
-      width / 4,
-      200,
-    )
+    text(`Press Spacebar to start Ghost Catcher Game`, width / 3, 100)
   } else if (isEnd && !isWin) {
     textSize(40)
     text(`You lose! You didn't catch any ghosts!`, width / 3, 100)
@@ -268,21 +212,17 @@ function draw() {
     text(`:)`, width / 2, 400)
     image(ghostHappy, 200, 100)
   }
-  if (isArduino && buttonCheck) {
-    mousePressed()
-  }
 }
 
 // check what was pressed
 function mousePressed() {
-  if (isStart && !isEnd) {
-    startGame()
-  } else if (!isStart && !isEnd && !isCarryingGhost && !isOverJar()) {
-    checkIfBlue()
-  } else if (isCarryingGhost && isOverJar()) {
+  if (!isStart && !isEnd && !isCarryingGhost) checkIfBlue()
+  console.log(mouseX)
+  console.log(mouseY)
+  if (isCarryingGhost && mouseX >= jarX1 && mouseX <=jarX2 && mouseY >= jarY1 && mouseY <= jarY2) {
     score++
     catches++
-    winSound()
+    noiseSynth.triggerAttackRelease('D4', '16n')
     isCarryingGhost = false
   }
 }
@@ -298,23 +238,21 @@ function changeBackgroundColor() {
 }
 
 function checkIfBlue() {
-  // does not count if currently carrying ghost
-  if (isCarryingGhost || isStart) return
-
   attempts++
   if (blues.includes(currentColor)) {
     // WIN
+    //score++
     isCorrect = true
     isCarryingGhost = true
-    winSound()
+    noiseSynth.triggerAttackRelease('A4', '16n')
     outMessage = 'W'
     serial.write(outMessage) // turn the light on UNO board
   } else {
     // MISS
-    isCarryingGhost = false
+    isCarryingGhost = false 
     score += score > 0 ? -1 : 0 // Decrease score
     isCorrect = false
-    missSound()
+    noiseSynth.triggerAttackRelease('G1', '16n')
     outMessage = 'L'
     serial.write(outMessage) // turn the light off UNO board
   }
@@ -329,53 +267,8 @@ function countdown() {
   if (timer == 0) endGame()
 }
 
-function changeCursor() {
-  if (isCarryingGhost && !isArduino) {
-    cursor('ghost-cursor.png')
-  } else if (!isArduino) {
-    cursor('net.png')
-  } else if (isCarryingGhost && isArduino) {
-    image(ghostCursor, x_position, y_position)
-  } else if (isArduino) {
-    image(netCursor, x_position, y_position)
-  }
-}
-
 function endGame() {
   isEnd = true
   isWin = score > 0 ? true : false
-  catchRate = Math.round((catches / attempts) * 100)
-}
-
-function startGame() {
-  isStart = false
-}
-
-function winSound() {
-  noiseSynth.triggerAttackRelease('A4', '16n')
-}
-
-function missSound() {
-  noiseSynth.triggerAttackRelease('G1', '16n')
-}
-
-// Checks if cursor is over the jar
-function isOverJar() {
-  if (
-    mouseX >= jarX1 &&
-    mouseX <= jarX2 &&
-    mouseY >= jarY1 &&
-    mouseY <= jarY2
-  ) {
-    return true
-  } else if (
-    isArduino &&
-    x_position >= jarX1 &&
-    x_position <= jarX2 &&
-    y_position >= jarY1 &&
-    y_position <= jarY2
-  ) {
-    return true
-  }
-  return false
+  catchRate = Math.round((catches/attempts) * 100)
 }
